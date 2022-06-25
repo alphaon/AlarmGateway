a1cf8115244716f75ccc6a7c84090057c0556022
alphaon <alphaon@users.noreply.github.com>
2022-06-25 16:28:26 +0500

    Initial commit

---
+++ AlarmGateWayLLR8000/AlarmGateWayLLR8000.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+
+
+}
+
+void loop() {
+  // if there's any serial available, read it:
+ // while (Serial.available() > 0) {
+
+
+      Serial.print("1234051100111111111111111111");
+
+    delay (5000);
+  //}
+}
